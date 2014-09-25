3b16940ca3653df5d34fcf5017d73b6fc7a7968b
Piyush Waradpande <pi5@umbc.edu>
2014-09-25 13:31:47 -0400

    Time functions practice

---
+++ CPP/time.cpp
@@ -0,0 +1,36 @@
+
+/* Reference as a return value */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+#include<iomanip>
+using std::setw;
+
+#include<ctime>
+
+int main() {
+
+    // Current time
+    time_t now = time(0);
+    cout << endl << "char* ctime():\t" << ctime(&now);
+    
+    tm* time_struct = gmtime(&now);
+    cout << endl << "gmtime";
+    cout << endl << setw(10) << "time_struct->tm_sec" << setw(10) << time_struct->tm_sec;
+    cout << endl << setw(10) << "time_struct->tm_min" << setw(10) << time_struct->tm_min;
+    cout << endl << setw(10) << "time_struct->tm_hour" << setw(10) << time_struct->tm_hour;
+    cout << endl << setw(10) << "time_struct->tm_mday" << setw(10) << time_struct->tm_mday;
+    cout << endl << setw(10) << "time_struct->tm_mon" << setw(10) << time_struct->tm_mon;
+    cout << endl << setw(10) << "time_struct->tm_year" << setw(10) << time_struct->tm_year;
+    cout << endl << setw(10) << "time_struct->tm_wday" << setw(10) << time_struct->tm_wday;
+    cout << endl << setw(10) << "time_struct->tm_year" << setw(10) << time_struct->tm_year;
+    cout << endl << setw(10) << "time_struct->tm_wday" << setw(10) << time_struct->tm_wday;
+    cout << endl << setw(10) << "time_struct->tm_yday" << setw(10) << time_struct->tm_yday;
+    cout << endl << setw(10) << "time_struct->tm_isdst" << setw(10) << time_struct->tm_isdst;
+   
+    cout << endl; 
+    return 0;   
+}
+
